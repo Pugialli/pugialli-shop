98bd87ac0d566161c3a19407bd9c4c0c1aec9ebb
Pugialli <joaopugialli@gmail.com>
2024-03-07 10:59:05 -0300

    update src folder

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,21 @@
 import { AppProps } from 'next/app'
 import { globalStyles } from '../styles/global'
 
+import logoImg from '../../public/pugialli-logo.svg'
+import { Container, Header } from '../styles/pages/app'
+import Image from 'next/image'
+
 globalStyles()
 
 export default function app({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Container>
+      <Header>
+        <div>
+          <Image src={logoImg.src} width={35} height={35} alt="" />
+        </div>
+      </Header>
+      <Component {...pageProps} />
+    </Container>
+  )
 }
